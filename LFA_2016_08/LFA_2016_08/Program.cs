e713e88e6b93d60cb3557af6545ba92e1cf887ca
thedpes <thedpes@outlook.com>
2016-08-14 07:45:30 +0545

    Initial project created

---
+++ LFA_2016_08/LFA_2016_08/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFA_2016_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
