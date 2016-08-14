8238a0d246f1af1702a0f7e6bdacfeec2d3d4356
thedpes <thedpes@gmail.com>
2016-08-14 08:18:30 +0545

    Update Program.cs

--- LFA_2016_08/LFA_2016_08/Program.cs
+++ LFA_2016_08/LFA_2016_08/Program.cs
@@ -7,12 +7,30 @@ using System.Threading.Tasks;
 namespace LFA_2016_08
 {
     class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
-            
-            Console.ReadKey();
+           int n = 0;
+            while (n < 3)
+            {
+                Console.WriteLine("Please Enter your Username: ");
+                string userName = Console.ReadLine();
+
+                Console.WriteLine("Please enter your password: ");
+                string password = Console.ReadLine();
+
+                if (userName == "dipesh" && password == "password")
+                {
+                    Console.WriteLine("Correct, {0}", userName);
+                    break;
+                }
+                n++;
+                Console.Clear();
+                Console.WriteLine("Incorrect, Please try again. try {0}/3", n);
+            }
+            Console.WriteLine("Thank you! press any key to exit");
+          Console.ReadKey();
         }
     }
 }
